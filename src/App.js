6b9653a97c0742eb617f6c8f38d1d678e86e3016
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-03-19 15:19:05 +0500

    third commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
-
 const url = 'https://course-api.com/react-tours-project'
 
 function App() {
